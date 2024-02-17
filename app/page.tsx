b2f1919684808dd104bc771c71c49e4f9ca0524f
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2024-02-18 00:29:41 +0100

    Build cursor

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 
 export default function Page() {
   return (
+    <div className="flex justify-center items-center text-center w-full h-[100vh]">
+      <h1 className="text-2xl text-white">Figma</h1>
+    </div>
 
-      <h1 className="text-5xl text-white">Figma</h1>
- 
+
   );
 }
